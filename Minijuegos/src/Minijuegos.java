e0932b6f045c9887584f1f181784101140e2915e
Siru <msrijanamp@gmail.com>
2022-01-18 17:53:57 +0100

    Añadido el menú principal
    Co-Authored-By: Axel <58775176+ItzAxxel@users.noreply.github.com>

--- Minijuegos/src/Minijuegos.java
+++ Minijuegos/src/Minijuegos.java
@@ -1,9 +1,45 @@
+//Programa hecho por Axel y Siru
+
+import java.util.Scanner;
 
 public class Minijuegos {
 
-	public static void main(String[] args) {
-		// TODO Apéndice de método generado automáticamente
+	static Scanner teclado = new Scanner(System.in);
 
+	public static void main(String[] args) {
+
+		/*
+		 * 
+		 * Adivina el numero y Combate
+		 * 
+		 * 
+		 */
+
+		menu();
+
+	}
+
+	public static int menu() {
+
+		boolean salir = false;
+		int opcion = 0; // Guardaremos la opcion del usuario
+
+		while (!salir) {
+
+			System.out.println(" Elige una opción: ");
+			System.out.println();
+			System.out.println(" 1.- Jugar");
+			System.out.println(" 2.- Información");
+			System.out.println(" 3.- Mostrar puntuaciones");
+			System.out.println(" 4.- Autores");
+			System.out.println(" 5.- Versión");
+			System.out.println(" 6.- Salir");
+			opcion = teclado.nextInt();
+			if (opcion == 6) {
+				salir = true;
+			}
+		}
+		return opcion;
 	}
 
 }
